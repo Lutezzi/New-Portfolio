d158a150530fb967b0dd1a9efd17d975bd912204
Yeet <68704550+Lutezzi@users.noreply.github.com>
2024-07-24 16:06:39 +0300

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -17,25 +17,10 @@ function About() {
             <div className="about-container">
                 <h2>About Me</h2>
                 <p>
-                Hi there! I'm Yiğit, a dedicated and enthusiastic web developer with a keen eye for detail and a passion for creating intuitive, 
-                user-friendly web applications. With a solid foundation in front-end technologies, I specialize in building responsive and engaging 
-                websites using HTML, CSS, and JavaScript. My journey began with a fascination for how the web works, and it has evolved into a 
-                full-fledged career where I constantly seek to learn and implement the latest web technologies.
-                </p>
-                <p>
-                I have hands-on experience with modern frameworks and libraries like React, which allows me to develop dynamic and 
-                efficient single-page applications. Additionally, I am proficient in working with back-end technologies such as 
-                Node.js to build robust and scalable server-side applications. My skills also extend to version control with Git, 
-                which helps me manage and collaborate on projects effectively.
-                </p>
-                <p>
-                I thrive on challenges and enjoy problem-solving through code. Whether it’s optimizing performance, enhancing user experience, 
-                or implementing new features, I’m always excited to tackle new problems and deliver high-quality solutions. When I'm not coding, 
-                you can find me exploring new technologies, contributing to open-source projects, or honing my skills through online courses and tutorials.
-                </p>
-                <p>
-                If you're looking for someone who is passionate about web development and committed to delivering exceptional results, 
-                feel free to reach out. I’m always open to new opportunities and collaborations that allow me to push the boundaries of what’s possible on the web.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
                 <div className="skills">
                     <h3>Techs</h3>
